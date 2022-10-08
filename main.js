6097cee04a77ffbf391ad660ffc4fa545b6bc1c6
kssus <kssus7@gmail.com>
2022-10-08 15:28:26 +0900

    js: navbar transparent

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 'use strict';
 
+// navbar transparent when It is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+// console.log(navbarHeight);
 
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight: ${navbarHeight}`)
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
