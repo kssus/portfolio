52827fc9b89588d3f0fc651dd119f9cf9de576c7
kssus <kssus7@gmail.com>
2022-10-13 16:48:50 +0900

    renewal & Section of  Appendix

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // navbar transparent when It is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-// console.log(navbarHeight);
 
 document.addEventListener('scroll', () => {
   // console.log(window.scrollY);
@@ -15,17 +14,29 @@ document.addEventListener('scroll', () => {
   }
 });
 
+// scrolling when tapping in navbar
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (e) => {
+  // console.log(e.target.dataset.link);
+  const target = e.target;
+  const link = target.dataset.link;
+  // console.log(link);
+  scrollIntoView(link);
+})
 // scrolling to pade home
 const home = document.querySelector('#home');
-const homeHeight = home.getBoundingClientRect().height;
 const homeContainer = document.querySelector('#home .section__container');
+const homeHeight = home.getBoundingClientRect().height;
+// console.log(homeHeight);
 document.addEventListener('scroll', () => {
-  // console.log(`homeHeight: ${homeHeight}`);
-  // console.log(window.scrollY);
-  homeContainer.style.opacity = 1 - (window.scrollY / homeHeight);
-});
+
+  homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
 // arrow up to home
 const arrowUp = document.querySelector('.arrow-up');
+
 document.addEventListener('scroll', () => {
   if(window.scrollY > homeHeight / 2) {
     arrowUp.classList.remove('invisible');
@@ -33,60 +44,67 @@ document.addEventListener('scroll', () => {
     arrowUp.classList.add('invisible');
   }
 })
+
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 })
 
-// scrolling when tapping in navbar
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-  if(link == null) {
-    return;
-  } else {
-    scrollIntoView(link);
-  }
-});
-
 // click "contact me" button
-const homeContact__btn = document.querySelector('.home__contact');
-homeContact__btn.addEventListener('click', () => {
+const homeBtn = document.querySelector('.home__contact');
+homeBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
-});
+})
 
 // utility funtion
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior: "smooth"});
-};
+  scrollTo.scrollIntoView({behavior : "smooth"});
+}
 
 // project filtering & animation
 const workCategories = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
+
+
+
 workCategories.addEventListener('click', (e) => {
-  // console.log(e.target.dataset.filter);
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  console.log(filter);
   if(filter == null) {
     return;
-  } 
-    // console.log(filter);
-    const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-    const selected = document.querySelector('.category__btn.active');
-    selected.classList.remove('active');
-    target.classList.add('active');
-    projectContainer.classList.add('anim-out');
-    setTimeout(() => {
-      projectContainer.classList.remove('anim-out');
-      projects.forEach((project) => {
-        // console.log(project.dataset.type);
-        if(filter === '*' || filter === project.dataset.type) {
-          project.classList.remove('invisible');
-        } else {
-          project.classList.add('invisible');
-        }
-    });
-    }, 300); 
+  }
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  const selected = document.querySelector('.category__btn.active');
+  selected.classList.remove('active');
+  target.classList.add('active');
+projectContainer.classList.add('anim-out');
+setTimeout(() => {
+  projectContainer.classList.remove('anim-out');
+  projects.forEach((project) => {
+    // console.log(project.dataset.type);
+    if(filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+    } else {
+      project.classList.add('invisible');
+    }
+  });
+}, 300);
 });
+
+
+  // console.log('-----------');
+  // for(let project of projects) {
+  //   console.log(project);
+
+  // };
+  
+  // console.log('------------');
+  // let project;
+  // for(let i = 0; i < projects.length; i++) {
+  //   console.log(projects[i]);
+
+  // };
+
+
   
